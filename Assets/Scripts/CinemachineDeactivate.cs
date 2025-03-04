5e24a1291ccc7e77630ccd1e537dcec50879757b
Carter Block <carterpblock@gmail.com>
2025-03-04 09:33:05 -0700

    Added the fly through camera.

--- Assets/Scripts/CinemachineDeactivate.cs
+++ Assets/Scripts/CinemachineDeactivate.cs
@@ -16,25 +16,33 @@ public class CinemachineDeactivate : MonoBehaviour
     [SerializeField]
     private float delayTime;
 
+    private bool bFinished;
+
     // Start is called before the first frame update
     void Start()
     {
      //   cVCamera = GetComponent<CinemachineVirtualCamera>();
         dolly = GetComponent<CinemachineDollyCart>();
+
+        bFinished = false;
     }
 
     public void Update()
     {
-        if (dolly.m_Position >= endPathUnits)
+        if (dolly.m_Position >= endPathUnits && !bFinished)
         {
             //TurnOffCamera();
             StartCoroutine(delayedTurnOff());
+            bFinished = true;
         }
     }
     public void TurnOffCamera()
     {
         Debug.Log("Camera should be disabled");
+
         cVCamera.enabled = false;
+
+        FindObjectOfType<LevelManager>().SpawnPlayer();
     }
 
     private IEnumerator delayedTurnOff()
