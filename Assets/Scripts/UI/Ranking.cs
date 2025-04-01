c4d9807089da9d10a8b2e508e6edf0df89ef555e
Yuxi Dai <87078520+HeChangeChina@users.noreply.github.com>
2025-04-01 09:52:58 -0600

    update ranking

--- Assets/Scripts/UI/Ranking.cs
+++ Assets/Scripts/UI/Ranking.cs
@@ -11,10 +11,6 @@ namespace GameUI
         public TextMeshProUGUI RankingText;
         public int HorseIndex;
 
-        private static float LastUpdateTime = 0;
-        private const float UPDATE_INTERVAL = 0.1f;
-        private static List<HorseController> RankingList;
-
         public void Update()
         {
             if(HorseController.Horses.Count < HorseIndex)
@@ -22,25 +18,7 @@ namespace GameUI
                 return;
             }
 
-            if(LastUpdateTime <= Time.realtimeSinceStartup - UPDATE_INTERVAL)
-            {
-                LastUpdateTime = Time.realtimeSinceStartup;
-                RankingList = new(HorseController.Horses);
-                RankingList.Sort(SortHorse);
-                RankingList.Reverse();
-            }
-
-            int index = RankingList.IndexOf(HorseController.Horses[HorseIndex]);
-            RankingText.text = (index + 1).ToString();
-        }
-
-        private static int SortHorse(HorseController a, HorseController b)
-        {
-            if(a.CheckPointIndex == b.CheckPointIndex)
-            {
-                return a.NextCheckPointDistance.CompareTo(b.NextCheckPointDistance);
-            }
-            return a.CheckPointIndex.CompareTo(b.CheckPointIndex);
+            RankingText.text = HorseController.Horses[HorseIndex].Ranking.ToString();
         }
     }
 
