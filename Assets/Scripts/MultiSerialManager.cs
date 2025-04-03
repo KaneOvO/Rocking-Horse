ac995e741a658a777583d8ca029fe1cc9595ac8c
OVO <lzx213@sina.com>
2025-04-02 21:15:59 -0600

    Update

--- Assets/Scripts/MultiSerialManager.cs
+++ Assets/Scripts/MultiSerialManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO.Ports;
 using UnityEngine;
+using System.Diagnostics;
+using System;
 
 public class MultiSerialManager : MonoBehaviour
 {
@@ -13,50 +15,77 @@ public class MultiSerialManager : MonoBehaviour
     {
         string[] ports = SerialPort.GetPortNames();
         int i = 0;
+
         foreach (string port in ports)
         {
             SerialPort sp = new SerialPort(port, baudRate);
-            sp.ReadTimeout = 1000;
-            sp.Open();
+            sp.ReadTimeout = 100;
+            sp.NewLine = "\n";
 
-            if (sp.IsOpen)
+            try
             {
-                if (sp.BytesToRead > 0)
-                {
-                    string data = sp.ReadLine();
-                    string[] dataParts = data.Split(',');
-                    //the data needs to match the arduino data format
-                    //currently it is set to 3 floats, but this may change in the future
-                    bool isArduino = dataParts.Length >= 3 && float.TryParse(dataParts[0], out float f) && float.TryParse(dataParts[1], out f) && float.TryParse(dataParts[2], out f);
+                sp.Open();
+                UnityEngine.Debug.Log($"Trying port: {port}");
 
-                    if (isArduino && i >= listeners.Length)
+                string receivedData = null;
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                while (stopwatch.ElapsedMilliseconds < 1000)
+                {
+                    try
                     {
-                        sp.Close();
-                        Debug.LogWarning("Not enough listeners for the number of Arduino devices connected.");
-                        break;
+                        receivedData = sp.ReadLine();
+                        if (!string.IsNullOrEmpty(receivedData))
+                        {
+                            UnityEngine.Debug.Log($"[{port}] Received: {receivedData}");
+                            break;
+                        }
                     }
+                    catch (TimeoutException)
+                    {
+                    }
+                }
+
+                if (receivedData != null)
+                {
+                    string[] dataParts = receivedData.Split(',');
+                    bool isArduino = dataParts.Length >= 3 &&
+                                     float.TryParse(dataParts[0], out _) &&
+                                     float.TryParse(dataParts[1], out _) &&
+                                     float.TryParse(dataParts[2], out _);
+
+                    UnityEngine.Debug.Log($"[{port}] Format valid: {isArduino}");
 
                     if (isArduino)
                     {
+                        if (i >= listeners.Length)
+                        {
+                            UnityEngine.Debug.LogWarning("Not enough listeners for connected devices.");
+                            sp.Close();
+                            break;
+                        }
+
                         sp.Close();
+
                         GameObject serialController = Instantiate(serialControllerPrefab);
                         if (serialController.TryGetComponent<SerialController>(out var controller))
                         {
                             controller.portName = port;
-                            controller.messageListener = listeners[i];
                             controller.baudRate = baudRate;
+                            controller.messageListener = listeners[i];
                             i++;
+                            serialController.SetActive(true);
+                            controller.enabled = true;
+                            UnityEngine.Debug.Log($"[{port}] Controller assigned to listener {i - 1}");
                         }
                     }
-                    else
-                    {
-                        sp.Close();
-                    }
                 }
-                else
-                {
-                    sp.Close();
-                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                UnityEngine.Debug.LogWarning($"Failed to read from port {port}: {ex.Message}");
+                if (sp.IsOpen) sp.Close();
             }
         }
     }
