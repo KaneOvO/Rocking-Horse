b5a24a616c68267e51072df2cc4f5604bfcb1623
OVO <lzx213@sina.com>
2025-04-15 21:30:02 -0600

    Update for singleton listener

--- Assets/Listeners.cs
+++ Assets/Listeners.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Listeners : MonoBehaviour
 {
+    public static Listeners instance;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            //重启页面
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+        }
     }
 }
