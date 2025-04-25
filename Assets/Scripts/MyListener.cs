213870abada51aa5962800a3f0619fbc6d0e9938
Noah <nrhart@ucsc.edu>
2025-04-25 01:22:49 -0600

    Playtest Ready

--- Assets/Scripts/MyListener.cs
+++ Assets/Scripts/MyListener.cs
@@ -54,53 +54,53 @@ public class MyListener : MonoBehaviour
         isSentConnected = false;
     }
 
-#if UNITY_EDITOR
-    private void Update()
-    {
-        // Simulate gyro movement
-        float gyroX = 0;
-        if (Input.GetKey(KeyCode.LeftArrow)) gyroX = -5f;
-        else if (Input.GetKey(KeyCode.RightArrow)) gyroX = 5f;
+//#if UNITY_EDITOR
+//    private void Update()
+//    {
+//        // Simulate gyro movement
+//        float gyroX = 0;
+//        if (Input.GetKey(KeyCode.LeftArrow)) gyroX = -5f;
+//        else if (Input.GetKey(KeyCode.RightArrow)) gyroX = 5f;
 
-        float rotationZ = 0;
-        int isBoosted = 0;
+//        float rotationZ = 0;
+//        int isBoosted = 0;
 
-        // Detect press start
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            pressStartTime = Time.time;
-            isPressing = true;
-            isBoosted = 1;
-        }
+//        // Detect press start
+//        if (Input.GetKeyDown(KeyCode.Space))
+//        {
+//            pressStartTime = Time.time;
+//            isPressing = true;
+//            isBoosted = 1;
+//        }
 
-        // While holding
-        if (Input.GetKey(KeyCode.Space))
-        {
-            isBoosted = 1;
-        }
+//        // While holding
+//        if (Input.GetKey(KeyCode.Space))
+//        {
+//            isBoosted = 1;
+//        }
 
-        // Detect release
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            isPressing = false;
-            float heldTime = Time.time - pressStartTime;
+//        // Detect release
+//        if (Input.GetKeyUp(KeyCode.Space))
+//        {
+//            isPressing = false;
+//            float heldTime = Time.time - pressStartTime;
 
-            if (heldTime >= 10f)
-            {
-                Debug.Log("Long press detected");
-                // MenuManager.Instance.BackToMenu(); // optional
-            }
-            else
-            {
-                Debug.Log("Short press detected");
-            }
+//            if (heldTime >= 10f)
+//            {
+//                Debug.Log("Long press detected");
+//                // MenuManager.Instance.BackToMenu(); // optional
+//            }
+//            else
+//            {
+//                Debug.Log("Short press detected");
+//            }
 
-            isBoosted = 0;
-        }
+//            isBoosted = 0;
+//        }
 
-        // Send simulated sensor data
-        string simulatedMsg = $"{gyroX},{rotationZ},{isBoosted}";
-        OnMessageArrived(simulatedMsg);
-    }
-#endif
+//        // Send simulated sensor data
+//        string simulatedMsg = $"{gyroX},{rotationZ},{isBoosted}";
+//        OnMessageArrived(simulatedMsg);
+//    }
+//#endif
 }
